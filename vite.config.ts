ab45d693d96ca929ca24068f1c80834571972e77
enes.cumali <enescumali@gmail.com>
2024-03-01 13:16:45 +0100

    base path update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir:  process.env.NODE_ENV === 'production' ? '/movie-list/' : '/',
+  base:  '/movie-list/',
   plugins: [vue()],
   resolve: {
     alias: {
